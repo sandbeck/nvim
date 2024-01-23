93554e0e1da60dd81186bc45a02beaf014c6ddbd
Christian Flinker Sandbeck <info@sandbeck.net>
2024-01-23 11:27:17 +0100

    Changes grep search keybinding, adds linenumbers and tweaks zen-mode

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -1,8 +1,13 @@
 return {
   "folke/zen-mode.nvim",
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    window = {
+      width = 80,
+      options = {
+        number = false,
+        relativenumber = false,
+
+      }
+    }
   }
 }
